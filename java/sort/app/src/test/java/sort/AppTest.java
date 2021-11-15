59777f98188c068f0c210455a0c126a5361b0af8
mahmoudabubaker9 <m7moud.abubaker.92@gmail.com>
2021-11-15 19:02:49 +0200

    finish code27

--- java/sort/app/src/test/java/sort/AppTest.java
+++ java/sort/app/src/test/java/sort/AppTest.java
@@ -29,4 +29,23 @@ class AppTest {
     int[] expectedOutput = {5,5,5,7,7,12};
     assertArrayEquals(expectedOutput,outputArray);
   }
+
+  @Test
+  public void margeSortTest(){
+    int[] array = {5,12,7,5,5,7};
+    int n=array.length;
+    int[] outputArray = sort.merge(array,0,n-1);
+    int[] expectedOutput = {5,5,5,7,7,12};
+    assertArrayEquals(expectedOutput,outputArray);
+  }
+
+  @Test
+  public void margeSortTest2(){
+    int[] array = {20,18,12,8,5,-2};
+    int n=array.length;
+    int[] outputArray = sort.merge(array,0,n-1);
+    int[] expectedOutput = {-2,5,8,12,18,20};
+    assertArrayEquals(expectedOutput,outputArray);
+  }
+
 }
